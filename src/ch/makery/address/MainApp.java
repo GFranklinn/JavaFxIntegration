76d3c3f853d887d3bbb29925e65d552b4abdf59a
Gabriel Franklin <gabrielfranklin567@gmail.com>
2023-02-06 15:14:32 -0300

    Added class model, list person and PersonOverview controller.

--- src/ch/makery/address/MainApp.java
+++ src/ch/makery/address/MainApp.java
@@ -2,8 +2,11 @@ package ch.makery.address;
 
 import java.io.IOException;
 
-
+import ch.makery.address.model.Person;
+import ch.makery.address.view.PersonOverviewController;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -30,14 +33,13 @@ public class MainApp extends Application {
 	public void initRootLayout() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MainApp.class.getResource("View/RootLayout.fxml"));
+			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
 			rootLayout = (BorderPane) loader.load();
 		
 			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			
-	}	catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 			
@@ -46,9 +48,11 @@ public class MainApp extends Application {
 	public void showPersonOverview() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MainApp.class.getResource("view/PersonOverview.fmxl"));
-			AnchorPane PersonOverview = (AnchorPane) loader.load();
-			rootLayout.setCenter(PersonOverview);
+			loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+			AnchorPane personOverview = (AnchorPane) loader.load();
+			rootLayout.setCenter(personOverview);
+			PersonOverviewController controller = loader.getController();
+			controller.setMainApp(this);
 	}   catch (IOException e) {
 			e.printStackTrace();
 	}
@@ -58,7 +62,28 @@ public class MainApp extends Application {
 			return primaryStage;
 	}
 
+	private ObservableList<Person> personData = FXCollections.observableArrayList();
+	
+	public MainApp() {
+		personData.add(new Person("Hans", "Muster"));
+		personData.add(new Person("Ruth", "Mueller"));
+		personData.add(new Person("Heinz", "Kurz"));
+		personData.add(new Person("Cornelia", "Meier"));
+		personData.add(new Person("Werner", "Meyer"));
+		personData.add(new Person("Lydia", "Kunz"));
+		personData.add(new Person("Anna", "Best"));
+		personData.add(new Person("Stefan", "Paul"));
+		personData.add(new Person("Martin", "Kobe"));
+	}
+	
+	public ObservableList<Person> getPersondata() {
+		return personData;
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
+	
+	
 }
